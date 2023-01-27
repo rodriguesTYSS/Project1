cbd00ce06c642f5a2177267847cebd02ca307f05
anilr <anilr@LAPTOP-Q9A42G2H>
2023-01-27 13:05:40 +0530

    fourth commit

--- src/Pack1/Class1.java
+++ src/Pack1/Class1.java
@@ -6,6 +6,8 @@ public class Class1 {
 		System.out.println("Hi this is new java Project..!");
 		System.out.println("this is the code for second push");
 		System.out.println("this is the code for third push");
+		System.out.println("this is the code for fourth push");
+		
 
 	}
 
