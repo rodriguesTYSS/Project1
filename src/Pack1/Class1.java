3d83785ddd5612c9de02de5cdd594567f188ab86
anilr <anilr@LAPTOP-Q9A42G2H>
2023-01-27 12:49:45 +0530

    third commit

--- src/Pack1/Class1.java
+++ src/Pack1/Class1.java
@@ -5,6 +5,7 @@ public class Class1 {
 	public static void main(String[] args) {
 		System.out.println("Hi this is new java Project..!");
 		System.out.println("this is the code for second push");
+		System.out.println("this is the code for third push");
 
 	}
 
