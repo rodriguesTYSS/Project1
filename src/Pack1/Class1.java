9bdcdd36d1c68594079d81eb01b0e931c6009822
anilr <anilr@LAPTOP-Q9A42G2H>
2023-01-27 12:31:44 +0530

    second commit

--- src/Pack1/Class1.java
+++ src/Pack1/Class1.java
@@ -4,6 +4,7 @@ public class Class1 {
 
 	public static void main(String[] args) {
 		System.out.println("Hi this is new java Project..!");
+		System.out.println("this is the code for second push");
 
 	}
 
